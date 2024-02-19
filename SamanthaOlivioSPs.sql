e61451c90e25319f2962d06d53835c64a09c0368
solivio14 <142524837+solivio14@users.noreply.github.com>
2024-02-19 17:11:38 -0500

    Add files via upload

---
+++ SamanthaOlivioSPs.sql
@@ -0,0 +1,22 @@
+USE DatabaseCreation;
+GO
+
+CREATE PROCEDURE GetCityWithHighestEventCount
+AS
+BEGIN
+    SELECT TOP 1 c.CityID, c.CityName, c.AvgTemp, COUNT(e.EventID) AS EventCount
+    FROM City c
+    INNER JOIN Event e ON c.CityID = e.CityID
+    GROUP BY c.CityID, c.CityName, c.AvgTemp
+    ORDER BY EventCount DESC;
+END;
+GO
+
+CREATE PROCEDURE FindCheapestEvents
+AS
+BEGIN
+    SELECT EventID, EventName, EventPrice
+    FROM Event
+    WHERE EventPrice = (SELECT MIN(EventPrice) FROM Event);
+END;
+GO
